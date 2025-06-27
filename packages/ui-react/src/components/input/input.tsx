de18af360bbd23c2a16c94d15e1b2fb2432bc253
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2025-06-27 18:22:04 +0200

    fix: migrate input and field to base-ui

--- packages/ui-react/src/components/input/input.tsx
+++ packages/ui-react/src/components/input/input.tsx
@@ -1,44 +1,34 @@
-import { forwardRef } from "react";
+import { Input as InputPrimitive } from "@base-ui-components/react/input";
 import { cn } from "../../lib/utils.js";
 
-export type InputProps = Omit<
-  React.InputHTMLAttributes<HTMLInputElement>,
-  "placeholder"
-> & {
-  // Make id required
-  id: string;
-};
+export type InputProps = Omit<InputPrimitive.Props, "placeholder">;
 
-const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type = "text", ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          `
-            flex
-            h-10 w-full px-3 py-2
-            rounded-lg
-            border border-gray-300
-            bg-gray-100 text-gray-dark
-            text-base
-            file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground
-            focus-visible:outline-none
-            focus-visible:border-gray-500
-            disabled:cursor-not-allowed
-            disabled:opacity-50
-          `,
-          className,
-        )}
-        ref={ref}
-        {...props}
-        // Explicitly set placeholder to empty string to avoid users ignoring
-        // the types and passing placeholder anyways.
-        placeholder=""
-      />
-    );
-  },
-);
-Input.displayName = "Input";
+const Input = ({ className, type = "text", ...props }: InputProps) => {
+  return (
+    <InputPrimitive
+      type={type}
+      className={cn(
+        `
+          flex
+          h-10 w-full px-3 py-2
+          rounded-lg
+          border border-gray-300
+          bg-gray-100 text-gray-dark
+          text-base
+          file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground
+          focus-visible:outline-none
+          focus-visible:border-gray-500
+          disabled:cursor-not-allowed
+          disabled:opacity-50
+        `,
+        className,
+      )}
+      {...props}
+      // Explicitly set placeholder to empty string to prevent users from
+      // ignoring the types and passing placeholder anyways.
+      placeholder=""
+    />
+  );
+};
 
 export { Input };
